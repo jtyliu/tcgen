371dc86e3477072c02035562223888401d3b130b
JoshuaTianYangLiu <josh.liu@student.tdsb.on.ca>
2021-06-14 20:04:12 -0400

    Create package install support

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+from tcgen._version import VERSION
+
+setuptools.setup(
+    name='tcgen',
+    version=VERSION,
+    author='Joshua Liu',
+    author_email='joshualiu@youarefantastic.com',
+    description='Test case generator. Quickly design and generate test cases without all the bulk',
+    keywords='program competitive programming codeforces',
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    license='MIT',
+    url='https://github.com/JoshuaTianYangLiu/tcgen',
+)
